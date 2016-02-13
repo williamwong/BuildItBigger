c962e5f849583150ba70e0fb0313ab8675d4137b
unknown <wwong@CASE-Fructose.wework.local>
2016-02-13 16:08:36 -0500

    Add Java library for jokes

---
+++ libjoker/src/main/java/com/udacity/gradle/builditbigger/jokes/Joker.java
@@ -0,0 +1,7 @@
+package com.udacity.gradle.builditbigger.jokes;
+
+public class Joker {
+    public String getJoke(){
+        return "What do you call an Amish guy with his hand in a horse\'s mouth? A mechanic.";
+    }
+}
